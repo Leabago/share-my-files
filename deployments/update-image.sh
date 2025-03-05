a78b8e67a38d308b3df50b3ddb8ab99424c93b7d
Leabago <43474556+Leabago@users.noreply.github.com>
2025-03-05 17:53:10 +0400

    temp files

--- deployments/update-image.sh
+++ deployments/update-image.sh
@@ -4,4 +4,4 @@ minikube image build -t share-my-files -f ./Dockerfile .
 echo "########################## apply deployment"
 kubectl apply -f ./deployments/deployment.yaml 
 echo "########################## restart deployment"
-kubectl rollout restart deployment --selector=app=share-my-files
+kubectl rollout restart deployment --selector=app=share-my-files -n=applications
--- ui/static/js/ddns_address.js
+++
@@ -1,1 +0,0 @@
-var ddnsAddress = 'http://localhost:8080';
